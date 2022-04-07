728d610c387e2bea664674879da93ac76a544758
Ayush783 <ayushc45xxx@gmail.com>
2022-04-07 19:42:02 +0530

    psychiatrist model created

---
+++ lib/home/model/psychiatrist.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class Psychiatrist extends Equatable {
+  const Psychiatrist(this.name, this.number);
+
+  Psychiatrist.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        number = json['number'];
+
+  const Psychiatrist.empty()
+      : name = '',
+        number = '';
+
+  final String? name;
+  final String? number;
+  //can add more fields like
+  //profilePicUrl
+  //qualification
+  //bio
+  //clients
+  //etc
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+final List<Map<String, dynamic>> mockData = [
+  {'name': 'Foo bar', 'number': '8822882282'},
+  {'name': 'bar foo', 'number': '9999882282'},
+  {'name': 'Foo foo', 'number': '8822345282'},
+  {'name': 'bar bar', 'number': '1273882482'},
+];
